d96bedaaf8c270ce84c1af72800f8c5c514f8617
rayssabuarque <brayssa17@gmail.com>
2024-05-20 08:06:54 -0300

    feat: construir a seção informativa inicial

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -7,11 +7,21 @@ const Home = () => {
     <>
         <NavBar></NavBar>
         <div className='container_home'>
+          <div className="header">
             <div className='titulo'>
                 <h1>Valorize o Cine</h1>
             </div>
 
             <button className='saiba_mais'>Saiba Mais</button>
+          </div>
+
+            <div className='container_sobreNos'>
+              <div className="texto">
+                <h1>Missão Artística Brasileira</h1>
+                <p>Entre os anos de 2002 a 2014, do total de ingressos de cinema vendidos no Brasil, bem menos da metade são produtos de produções nacionais. (Ancine, )</p>
+                <p>O Valorize o Cine é uma iniciativa que busca incentivar a população a consumir um maior número de filmes brasileiros, e estimular uma reconexão das pessoas com o cinema nacional.</p>
+              </div>
+            </div>
         </div>
     </>
   );
