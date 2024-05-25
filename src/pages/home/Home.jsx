898f10fda4fa5a2f46cb4844daa1abc289730c2d
rayssabuarque <brayssa17@gmail.com>
2024-05-25 17:52:00 -0300

    adicionando texto sobre a história do cinema

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -9,10 +9,14 @@ import NavBar from '../../components/navBar/NavBar';
 import CheckList from '../../components/checkList/CheckList';
 // dados
 import indicacoesFilmes from '../../data/indicacoes';
+import conteudo from '../../data/conteudo';
 // imagens
 import faixa from '../../assets/faixa.png'
 
 const Home = () => {
+  // coletando parágrafos de conteúdo
+  const textoConteudo = conteudo[0].texto.split('\n');
+
   // gatilho de carregamento de seções
   const [loadCheckList, setLoadCheckList] = useState([false, false, false]);
 
@@ -64,11 +68,12 @@ const Home = () => {
             <div className="container_historiaCinema">
               <div className="texto">
                 <h1>Um pouco da história do cinema no Brasil</h1>
-                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus cursus vitae diam quis malesuada. Nulla pellentesque, neque sit amet feugiat hendrerit, magna mi molestie ex, dapibus sagittis nibh magna nec nisl. Duis pulvinar est nulla, at placerat erat efficitur eget. Quisque nec leo at libero facilisis lobortis. Vivamus vestibulum tempor sem, sed bibendum dolor euismod et. Aenean id magna eu lectus aliquam sodales. Ut quis massa nisi.</p>
-                <p>Sed in sem pulvinar, luctus risus non, hendrerit risus. Nulla volutpat consequat nulla vel congue. Pellentesque metus eros, mollis in pellentesque sit amet, tincidunt eu dolor. Morbi mollis odio ut mauris mollis, ut porta elit imperdiet. Sed in placerat ligula, id finibus ante. Nunc nec elementum justo. Sed convallis, sapien ut faucibus viverra, magna nisi tincidunt mi, quis facilisis tellus ipsum condimentum felis. Sed enim ante, vehicula eu massa at, gravida varius est. Pellentesque egestas eu nibh ac elementum. Nulla pulvinar laoreet tortor, at faucibus felis eleifend quis. In metus eros, tempor a rhoncus quis, interdum nec elit.</p>
+                {textoConteudo.map((paragrafo) => (
+                  <p>{paragrafo}</p>
+                ))}
               </div>
               <div className="imagem">
-                <img src="https://img.freepik.com/fotos-gratis/superficie-abstrata-e-texturas-de-parede-de-pedra-branca-de-concreta_74190-8189.jpg" alt="Fotografia da Cinelândia, no Rio de Janeiro." />
+                <img src={conteudo[0].imagem} alt={conteudo[0].altTextImg} />
               </div>
             </div>
 
